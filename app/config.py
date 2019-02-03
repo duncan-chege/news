a3e17f0e142baad52e31b0ad1a384fc8329418da
dunyung1 <dshege4@gmail.com>
2019-02-03 21:04:01 +0300

    add a news API

--- app/config.py
+++ app/config.py
@@ -2,9 +2,8 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
 
-
+    NEWS_API_BASE_URL="https://newsapi.org/v2/top-headlines?country=us&apiKey={}"
 
 class ProdConfig(Config):
     '''
