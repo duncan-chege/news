35b530fc6cabf40529db829eec4f8fa2848482ee
dunyung1 <dshege4@gmail.com>
2019-02-05 15:20:15 +0300

    change prodconfig docstring

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config:
     
 class ProdConfig(Config):
     '''
-    Production  configuration child class
+    Production This is a configuration child class
 
     Args:
         Config: The parent configuration class with General configuration settings
