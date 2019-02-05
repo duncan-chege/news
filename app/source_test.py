de0d42ba3976b6cb8b5d1e28c73684178e8ed609
dunyung1 <dshege4@gmail.com>
2019-02-05 15:17:08 +0300

    change source file

--- app/source_test.py
+++ app/source_test.py
@@ -12,7 +12,7 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Sources("fox-news",'Fox News','Joaqin Guzman is dangerous',"https://www.foxnews.com/us/el-chapo-accused-of-drugging-rapping-girls-as-young-as-13-in-court-documents")
+        self.new_source = Sources("foxnews",'Fox News','Joaqin Guzman is dangerous',"https://www.foxnews.com/us/el-chapo-accused-of-drugging-rapping-girls-as-young-as-13-in-court-documents")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Sources))
